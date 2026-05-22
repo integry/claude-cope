1297c923641cfd571c5b56c18034db3f1bb1e161
Rinalds Uzkalns <livecart@gmail.com>
2026-05-22 13:31:59 +0100

    Load Courier fallback in share renderer

--- apps/frontend/src/shareCardRenderMain.tsx
+++ apps/frontend/src/shareCardRenderMain.tsx
@@ -1,4 +1,6 @@
 import { createRoot } from "react-dom/client";
+import "@fontsource/courier-prime/latin-400.css";
+import "@fontsource/courier-prime/latin-700.css";
 import "./index.css";
 import ShareCardRenderPage from "./components/ShareCardRenderPage";
 
